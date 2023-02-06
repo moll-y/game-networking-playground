deb5418f290d6373b23a3ad1c59af436158de632
moll-y <mol.tired@gmail.com>
2023-02-06 01:09:50 -0500

    feat: add udp server and client
    server: send packets to the broadcast address.
    client: read packets from the server.

---
+++ udp-broadcast/src/client/main.rs
@@ -0,0 +1,25 @@
+//! UDP client
+
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    // The listener port (clients) and the broadcaster port (server) must be the same
+    let socket = UdpSocket::bind("0.0.0.0:6000").expect("couldn't bind IP address");
+
+    let mut buf = [5; 5];
+    println!("waiting for a message");
+    let (number_of_bytes, src_address) = socket.recv_from(&mut buf).expect("didn't receive data");
+    let buf = &mut buf[..number_of_bytes];
+
+    let socket_ip = socket.local_addr().unwrap();
+    println!("{}: {} sent a message: {:?}", socket_ip, src_address, buf);
+
+    // Send ten packets to the server
+    for x in 1..11 {
+        socket
+            .send_to(&[x; 5], &src_address)
+            .expect("couldn't send message");
+    }
+
+    Ok(())
+}
---
+++ udp-broadcast/src/server/main.rs
@@ -0,0 +1,29 @@
+//! UDP server
+
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    // I want a dynamic port from the stack, that's why the port :0 is used
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind IP address");
+    // When enable, this socket is allow to send packets to a broadcast address
+    socket
+        .set_broadcast(true)
+        .expect("set_broadcast call failed");
+
+    let mut buf = [0; 5];
+    // This is the `Broadcast Address`, any port number can be used here, but the udp clients must
+    // match this port number if they want to receive the packets sent by us.
+    socket
+        .send_to(&buf, "255.255.255.255:6000")
+        .expect("couldn't send message");
+
+    let socket_ip = socket.local_addr().unwrap();
+
+    println!("waiting for messages");
+    while let Ok((number_of_bytes, src_address)) = socket.recv_from(&mut buf) {
+        let buf = &mut buf[..number_of_bytes];
+        println!("{}: {} sent a message: {:?}", socket_ip, src_address, buf);
+    }
+
+    Ok(())
+}
